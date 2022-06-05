54d55fe238510433673ca89d865f79cda98f3f0a
ezzelkady1993 <43051301+ezzelkady1993@users.noreply.github.com>
2022-06-05 20:23:06 +0200

    First day tasks task 1 & 2

---
+++ task1.php
@@ -0,0 +1,13 @@
+<?php
+
+$num1 = 10; 
+$num2 = 20;
+
+
+$num1 = $num1 + $num2;
+$num2 = $num1 - $num2;
+$num1 = $num1 - $num2; 
+
+echo "num1 = ", $num1, ", ", "num2 = ", $num2;
+
+?>
---
+++ task2.php
@@ -0,0 +1,8 @@
+<?php
+
+$var1 = "welcome to "; 
+$var2 = "session 2";
+
+echo $var1 . $var2;
+
+?>
